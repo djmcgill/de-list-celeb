d07e0062d7a93b9a28e4cb773f41ae611580b5e4
David McGillicuddy <contact@djmcgill.co.uk>
2019-03-18 13:34:15 +0000

    set up env vars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use egg_mode;
+use std::env;
+fn main() -> Result<(), Box<std::error::Error>> {
+    let consumer_key = env::var("CONSUMER_KEY")?;
+    let consumer_secret = env::var("CONSUMER_SECRET")?;
     println!("Hello, world!");
+    Ok(())
 }
